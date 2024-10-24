629d879c10e4609a5bd56df5d3a320f4d2cb15b1
YoghurSnow <139123453+YoghurSnow@users.noreply.github.com>
2024-10-24 23:35:38 +0800

    Add files via upload

--- FileName.cpp
+++ FileName.cpp
@@ -1,19 +1,59 @@
-//c++
 #include<iostream>
-#include<cmath>
 using namespace std;
 
+const int N = 100;
+int str1[N], str2[N], str3[N];
+
 int main() {
-	//全部得用double，准确精度，包括a,b,c
-	double a, b, c;
-	cin >> a >> b >> c;
-	//scanf("%lf%lf%lf",&a,&b,&c);
-	double p = (a + b + c) / 2.0;//此处得是2.0
-	double s2 = p * (p - a) * (p - b) * (p - c);
-	double s = sqrt(s2);
-	//求根函数aqrt();
+	int n;
+	int tmp = 0, tmp2 = 0, num1 = 0, num2 = 0;
+	cin >> n;
+	
+	//输入
+	for (int i = 0; i < n; i++) {
+		cin >> str1[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> str2[i];
+		str3[i] = str2[i];
+	}
 
-	printf("%.1lf", s);
 
+	if (str1[0] > str3[0] && str1[n - 1] == str3[n - 1]) {
+		//插入排序
+		for (int i = 0; i < n; i++) {
+			if (str2[i] > str2[i + 1]) {
+				tmp = str2[i + 1];
+				num1 = i + 1;
+				break;
+			}
+
+		}
+		for (int i = 0; i < num1; i++) {
+			int end = i;
+			int tem = str2[end + 1];
+			while (end >= 0) {
+				if (tem < str2[end]) {
+					str2[end + 1] = str2[end];
+					end--;
+				}
+				else {
+					break;
+				}
+			}
+			str2[end + 1] = tem;
+		}
+
+		cout << "Insertion Sort" << endl;
+	}
+	else {
+		cout << "Heap Sort" << endl;
+	}
+	for (int i = 0; i < n; i++) {
+		cout << str2[i];
+		if (i < n - 1) {
+			cout << " ";
+		}
+	}
 	return 0;
 }
